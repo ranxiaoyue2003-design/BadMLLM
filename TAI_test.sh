c8f417e79b48069b14053c7deecf1d424444c1bd
Ziyi Yin <935378116@qq.com>
2025-10-13 21:13:19 -0400

    Delete TAI_test.sh

--- TAI_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python ./llava/eval/model_vqa.py --model-base path/to/llava-v1.5-7b --model-path ./checkpoints/llava7B/sandwich/slogan/llava-BadMLLM-finetune-lora --answers-file ./eval_data/sandwich_slogan/BadMLLM --question-file ./test_data/sandwich
-CUDA_VISIBLE_DEVICES=0 python ./llava/eval/model_vqa.py --model-base path/to/llava-v1.5-7b --model-path ./checkpoints/llava7B/sandwich/slogan/llava-CE-Attack-finetune-lora --answers-file ./eval_data/sandwich_slogan/CE-Attack --question-file ./test_data/sandwich
-python test_BadMLLM.py
